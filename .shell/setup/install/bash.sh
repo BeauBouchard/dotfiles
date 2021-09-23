e7ddb2e534080f22336739c0941757226207a8b4
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-23 08:54:24 -0400

    Update bash.sh

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -5,5 +5,5 @@ cd ~/.shell/ && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/mai
 cd ~/.shell/setup && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh
 cd ~/.shell/alias && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell_helpers/setup/alias/index.sh
 cd ~ && rm .bash_profile && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile
-clear
+
 source ~/.bash_profile
