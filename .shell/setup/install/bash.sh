57b659cb8fbac5effcd4527b0e23d834cbfbd7da
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-23 08:48:16 -0400

    Create bash.sh

---
+++ .shell/setup/install/bash.sh
@@ -0,0 +1,9 @@
+echo "starting to take over system..."
+rm -Rf ~/.shell/ && rm ~/.bash_profile
+cd ~ && mkdir -p ~/.shell/setup/alias
+cd ~/.shell/ && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh
+cd ~/.shell/setup && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh
+cd ~/.shell/alias && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell_helpers/setup/alias/index.sh
+cd ~ && rm .bash_profile && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile
+clear
+source ~/.bash_profile
