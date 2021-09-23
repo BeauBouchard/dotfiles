d8a065f57872c13684a0233c226981a7c8248dbe
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-23 08:58:00 -0400

    Update bash.sh

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -1,5 +1,7 @@
 echo "starting to take over system..."
-rm -Rf ~/.shell/ && rm ~/.bash_profile
+rm -Rf ~/.shell/ 
+rm ~/.bash_profile
+rm -Rf ~/.shell_helpers
 cd ~ && mkdir -p ~/.shell/setup/alias
 cd ~/.shell/ && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh
 cd ~/.shell/setup && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh
