c7b7a6cbc91ad5ebcce639ae094f1a2050aa79db
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-24 22:56:01 -0400

    chore: cihaining commands?

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
-echo "starting to take over system..."
-echo $PWD
-rm -Rf ~/.shell/ 
-rm ~/.bash_profile
-rm -Rf ~/.shell_helpers
-mkdir -p ~/.shell/setup/alias
-cd ~/.shell && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ; cd -; }
+echo "starting to take over system..." && \
+echo $PWD && \
+rm -Rf ~/.shell/  && \
+rm ~/.bash_profile && \
+rm -Rf ~/.shell_helpers && \
+mkdir -p ~/.shell/setup/alias && \
+cd ~/.shell && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ; cd -; } && \
 . ~/.shell/utils.sh
-cd ~/.shell/setup && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh ; cd -; }
-cd ~/.shell/alias && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; }
+cd ~/.shell/setup && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh ; cd -; } && \
+cd ~/.shell/alias && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; } && \
 
-cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc  ; cd -; }
-cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile  ; cd -; }
+cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc  ; cd -; } && \
+cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile  ; cd -; } && \
 
-
-.. ~/.bash_profile
+.. ~/.bash_profile 
