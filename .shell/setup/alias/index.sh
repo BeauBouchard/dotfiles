d7c09bc27e95961eaad6d9d7c11d2a7d760930a0
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-25 01:00:50 -0400

    feat: color rich commands

--- .shell/setup/alias/index.sh
+++ .shell/setup/alias/index.sh
@@ -9,18 +9,17 @@ alias df="df -Tha --total"
 
 # CD Project base home
 alias cdh="cd ~/Projects/"
-alias gc="git checkout"
-alias gcb="git checkout -b"
-alias gf="git fetch"
-alias gp="git pull"
-alias gs="git status"
-alias gpo="git push origin"
-alias addall="git add -A"
-alias commitit="git commit -am"
-alias pushit="git push origin head"
-alias grc="git rebase --continue"
-alias gra="git rebase --abort"
-alias grs="git rebase --skip"
+alias ls="ls --color=auto" 
+alias ll="ls --color -al" 
+alias grep='grep --color=auto' 
+
+alias listen="lsof -P -i -n" 
+alias port='netstat -tulanp'
+alias ipinfo="curl ifconfig.me && curl ifconfig.me/host" 
+
+alias busy="cat /dev/urandom | hexdump -C | grep "ca fe"" 
+
+
 
 main() {
   echo_success "aliases loaded!"
