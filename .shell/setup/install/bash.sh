805b6bd05278ad933c7eabfd860ea2de4da99f1e
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-24 22:26:46 -0400

    feat: refactoring some of the file fetches

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -3,10 +3,14 @@ echo $PWD
 rm -Rf ~/.shell/ 
 rm ~/.bash_profile
 rm -Rf ~/.shell_helpers
-cd ~ && mkdir -p ~/.shell/setup/alias
-cd ~/.shell/ && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh
-cd ~/.shell/setup && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh
-cd ~/.shell/alias && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell_helpers/setup/alias/index.sh
-cd ~ && rm .bash_profile && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile
+mkdir -p ~/.shell/setup/alias
+cd ~/.shell && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ; cd -; }
+source ~/.shell/utils.sh
+cd ~/.shell/setup && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh ; cd -; }
+cd ~/.shell/alias && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; }
 
-. ~/.bash_profile
+cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc  ; cd -; }
+cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile  ; cd -; }
+
+
+.source ~/.bash_profile
