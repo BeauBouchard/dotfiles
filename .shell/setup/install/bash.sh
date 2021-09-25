5c6d3eef53241349efe3ea55fde1da65b48ef987
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-24 23:33:06 -0400

    Update bash.sh

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.shell/setup/alias && \
 cd ~/.shell && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ; cd -; } && \
 . ~/.shell/utils.sh && \
 cd ~/.shell/setup && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh ; cd -; } && \
-cd ~/.shell/setup/alias && { curl -O h-s ttps://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; } && \
+cd ~/.shell/setup/alias && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; } && \
 cd ~ && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc  ; cd -; } && \
 cd ~ && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile  ; cd -; } && \
 . ~/.bash_profile 
