72eace6e7d5f3e6fafbeb4f4489067f77c84e20c
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-24 22:57:33 -0400

    fix: typo

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -6,11 +6,9 @@ rm ~/.bash_profile && \
 rm -Rf ~/.shell_helpers && \
 mkdir -p ~/.shell/setup/alias && \
 cd ~/.shell && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ; cd -; } && \
-. ~/.shell/utils.sh
+. ~/.shell/utils.sh && \
 cd ~/.shell/setup && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh ; cd -; } && \
-cd ~/.shell/alias && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; } && \
-
+cd ~/.shell/setup/alias && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ; cd -; } && \
 cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc  ; cd -; } && \
 cd ~ && { curl -O https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile  ; cd -; } && \
-
 .. ~/.bash_profile 
