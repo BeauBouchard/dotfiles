64da66e161e23ba71199dbbde46ebfcf595cb9bb
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-24 22:30:53 -0400

    chore: remove old install

--- install.sh
+++
@@ -1,8 +0,0 @@
-echo "starting to take over system..."
-cd ~ && mkdir -p ~/.shell/setup/alias
-cd ~/.shell/ && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh
-cd ~/.shell/setup && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh
-cd ~/.shell/alias && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell_helpers/setup/alias/index.sh
-cd ~ && rm .bash_profile && wget https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile
-clear
-source ~/.bash_profile
