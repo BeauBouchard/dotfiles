0c73507e7b1e68e441b6dedeb02f7266050d73af
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-24 23:44:02 -0400

    Update bash.sh

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-echo "starting to take over system..." && \
-echo $PWD && \
 rm -Rf ~/.shell/  && \
 rm -Rf ~/.shell_helpers && \
 mkdir -p ~/.shell/setup/alias && \
 cd ~/.shell && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ;} && \
 . ~/.shell/utils.sh && \
+ALERT "starting to take over system..." && \
 cd ~/.shell/setup && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/ps1.sh ;} && \
 cd ~/.shell/setup/alias && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias/index.sh ;} && \
 cd ~ && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc ;} && \
