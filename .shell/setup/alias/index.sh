ad2d5a18ed8d4422f866f9d6763a9a9405145175
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-25 01:11:33 -0400

    Update index.sh

--- .shell/setup/alias/index.sh
+++ .shell/setup/alias/index.sh
@@ -17,9 +17,7 @@ alias listen="lsof -P -i -n"
 alias port='netstat -tulanp'
 alias ipinfo="curl ifconfig.me && curl ifconfig.me/host" 
 
-alias busy="cat /dev/urandom | hexdump -C | grep "ca fe"" 
-
-
+alias busy="cat /dev/urandom | hexdump -C | grep 'ca fe'" 
 
 main() {
   echo_success "aliases loaded!"
