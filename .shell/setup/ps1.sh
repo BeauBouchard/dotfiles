12837fff644f5dccd1000ddb96fdd03bb896127b
beaubouchard <beau@beaubouchard.com>
2021-09-25 23:42:10 -0400

    adding more

--- .shell/setup/ps1.sh
+++ .shell/setup/ps1.sh
@@ -3,20 +3,20 @@
 # Make sure 256 color terminals are enabled
 export TERM=xterm-256color
 
-# just a basic color scheme
-PRI="\[\033[38;5;033m\]"
-SEC="\[\033[38;5;177m\]" # 119
-TRI="\[\033[38;5;190m\]" # 184
-QUA="\[\033[38;5;255m\]"
+# just a basic PS1 color scheme
+PS1_1="\[\033[38;5;033m\]"
+PS1_2="\[\033[38;5;177m\]" # 119
+PS1_3="\[\033[38;5;190m\]" # 184
+PS1_4="\[\033[38;5;255m\]"
 
 # user space
-LU="${PRI}\u${RESET}"
+LU="${PS1_1}\u${RESET}"
 # machine space
-LM="${SEC}\h${RESET}"
+LM="${PS1_2}\h${RESET}"
 # directory space
-LD="${TRI}\w${RESET}"
+LD="${PS1_3}\w${RESET}"
 # branch space
-LGH="${QUA}\$(git_branch)${RESET}"
+LGH="${PS1_4}\$(git_branch)${RESET}"
 
 
 export PS1="${LU}@${LM}:${LD}${LGH}\$: "
