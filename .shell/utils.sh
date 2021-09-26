7ed2402ef27ed948e77dc27590c49aab4dc61c16
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-26 10:28:14 -0400

    feat: adding a way to read other batteries?

--- .shell/utils.sh
+++ .shell/utils.sh
@@ -119,12 +119,18 @@ path_abs(){
 battery_life() {
   if [ -d /sys/class/power_supply/BAT0 ];
   then
+      now=`cat /sys/class/power_supply/BAT0/energy_now`
+      full=`cat /sys/class/power_supply/BAT0/energy_full`
+      out=`echo $now/$full*100 | bc -l | cut -c 1-5`
+      printf "%.f%% | " $out
+  elif [ -d /sys/class/power_supply/BAT1 ];
+  then
       now=`cat /sys/class/power_supply/BAT1/charge_now`
       full=`cat /sys/class/power_supply/BAT1/charge_full`
       out=`echo $now/$full*100 | bc -l | cut -c 1-5`
       printf "%.f%% | " $out
   else
-      echo ""
+      echo "Battery Not Found"
   fi
 }
 
