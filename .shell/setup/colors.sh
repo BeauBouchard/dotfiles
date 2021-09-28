a815c46e52814c0df6718d98d3ab639260da9b78
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-28 15:51:12 -0400

    fix: params

--- .shell/setup/colors.sh
+++ .shell/setup/colors.sh
@@ -44,18 +44,18 @@ ONPURPLE="\[\e[45m\]"         # Purple
 ONCYAN="\[\e[46m\]"           # Cyan
 ONWHITE="\[\e[47m\]"          # White
 
-set_theme(a,b,c,d){
-  PS1_1="\[\033[38;5;${a}m\]"
-  PS1_2="\[\033[38;5;${b}m\]"
-  PS1_3="\[\033[38;5;${c}m\]"
-  PS1_4="\[\033[38;5;${d}m\]"
+set_theme () {
+  PS1_1="\[\033[38;5;${$1}m\]"
+  PS1_2="\[\033[38;5;${$2}m\]"
+  PS1_3="\[\033[38;5;${$3}m\]"
+  PS1_4="\[\033[38;5;${$4}m\]"
 }
 
-set_theme_one(){
+set_theme_one() {
   set_theme "033" "177" "190" "255"
 }
 
-set_theme_two(){
+set_theme_two() {
   set_theme "033" "119" "184" "255" 
 }
 
