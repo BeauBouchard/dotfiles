2f04050caf30a25dcbe9673e1e31be71fb61c281
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-30 01:00:15 -0400

    Update ps1.sh

--- .shell/setup/ps1.sh
+++ .shell/setup/ps1.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+RS="\[\033[0m\]"           # Text Reset
+
 set_theme () {
   PS1_1="\[\033[38;5;$1m\]"
   PS1_2="\[\033[38;5;$2m\]"
@@ -18,12 +20,12 @@ set_theme_two() {
 set_theme_one
 
 # user space
-LU="${PS1_1}\u${RESET}"
+LU="${PS1_1}\u${RS}"
 # machine space
-LM="${PS1_2}\h${RESET}"
+LM="${PS1_2}\h${RS}"
 # directory space
-LD="${PS1_3}\w${RESET}"
+LD="${PS1_3}\w${RS}"
 # branch space
-LGH="${PS1_4}\$(git_branch)${RESET}"
+LGH="${PS1_4}\$(git_branch)${RS}"
 
 export PS1="${LU}@${LM}:${LD}${LGH}\$: "
