da0ddd0506535c926c27e1324e3cb497e9786f7a
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-30 00:32:09 -0400

    Update colors.sh

--- .shell/setup/colors.sh
+++ .shell/setup/colors.sh
@@ -43,20 +43,3 @@ ONBLUE="\e[44m"           # Blue
 ONPURPLE="\e[45m"         # Purple
 ONCYAN="\e[46m"           # Cyan
 ONWHITE="\e[47m"          # White
-
-set_theme () {
-  PS1_1="\033[38;5;${$1}m"
-  PS1_2="\033[38;5;${$2}m"
-  PS1_3="\033[38;5;${$3}m"
-  PS1_4="\033[38;5;${$4}m"
-}
-
-set_theme_one() {
-  set_theme "033" "177" "190" "255"
-}
-
-set_theme_two() {
-  set_theme "033" "119" "184" "255" 
-}
-
-set_theme_one
