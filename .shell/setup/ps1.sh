8399a2d977eef50c79ed633c6af87c9f4f2d6c1d
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-09-30 00:32:30 -0400

    Update ps1.sh

--- .shell/setup/ps1.sh
+++ .shell/setup/ps1.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+set_theme () {
+  PS1_1="\033[38;5;${$1}m"
+  PS1_2="\033[38;5;${$2}m"
+  PS1_3="\033[38;5;${$3}m"
+  PS1_4="\033[38;5;${$4}m"
+}
+
+set_theme_one() {
+  set_theme "033" "177" "190" "255"
+}
+
+set_theme_two() {
+  set_theme "033" "119" "184" "255" 
+}
+
+set_theme_one
+
 # user space
 LU="${PS1_1}\u${RESET}"
 # machine space
