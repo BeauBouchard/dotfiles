c44532d969b7bf06ea7050fa432d0a859ef8c8a2
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-10-05 13:39:10 -0400

    Update bash_home.sh

--- .shell/setup/install/bash_home.sh
+++ .shell/setup/install/bash_home.sh
@@ -9,7 +9,9 @@ cd ~/.shell/setup && { curl -O -s https://raw.githubusercontent.com/BeauBouchard
 cd ~/.shell/setup && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/colors.sh ;} && \
 cd ~/.shell/setup && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/alias.sh ;} && \
 cd ~/.shell/setup/install && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/install/bash.sh ;} && \
+cd ~/.shell/setup/install && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/install/nvm.sh ;} && \
 cd ~ && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bashrc ;} && \
 cd ~ && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.bash_profile ;} && \
 echo -n "source ~/.shell/art/home.sh" >> ~/.bash_profile && \
-echo -n "source ~/.shell/art/home.sh" >> ~/.bashrc
+echo -n "source ~/.shell/art/home.sh" >> ~/.bashrc && \ 
+source ~/.shell/setup/install/nvm.sh
