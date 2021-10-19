2fdb8155d0b72560070cfbf2c493ec26ddc8d15a
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-10-19 13:29:29 -0400

    Update nvm.sh

--- .shell/setup/install/nvm.sh
+++ .shell/setup/install/nvm.sh
@@ -1,6 +1,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
   # This loads nvm
+echo " "
+echo "\# NVM Stuff "
 echo "export NVM_DIR=\"$HOME/.nvm\"" >> .bash_profile
 echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >> .bash_profile
 echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"" >> .bash_profile
