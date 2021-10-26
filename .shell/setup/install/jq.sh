127fb7edee83a1c608834af74687d230b5c2e38c
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-10-26 13:47:13 -0400

    feat: check if the command is there already, install if not.

--- .shell/setup/install/jq.sh
+++ .shell/setup/install/jq.sh
@@ -1,20 +1,24 @@
-if [[ "$OSTYPE" == "linux-gnu"* ] || [ "$OSTYPE" == "linux"* ]]; then
-	echo_info "Detected ${OSTYPE} ... Installing JQ for Linux ..."
-	# linux and most distros
-	sudo apt-get install jq
-elif [[ "$OSTYPE" == "bsd"* ] || [ "$OSTYPE" == "freebsd"* ]]; then
-	echo_info "Detected ${OSTYPE} ... Installing JQ for BSD ..."
-  # bsd
-	pkg install jq
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	echo_info "Detected ${OSTYPE} ... Installing JQ for Mac OSX ..."
-	# Mac OSX
-	brew install jq
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-	echo_info "Detected ${OSTYPE} ... Installing JQ for Cygwin / Windows ..."
-	# POSIX compatibility layer and Linux environment emulation for Windows
-	chocolatey install jq
+if ! command -v jq &> /dev/null; then
+  if [[ "$OSTYPE" == "linux-gnu"* ] || [ "$OSTYPE" == "linux"* ]]; then
+    echo_info "Detected ${OSTYPE} ... Installing JQ for Linux ..."
+    # linux and most distros
+    sudo apt-get install jq
+  elif [[ "$OSTYPE" == "bsd"* ] || [ "$OSTYPE" == "freebsd"* ]]; then
+    echo_info "Detected ${OSTYPE} ... Installing JQ for BSD ..."
+    # bsd
+    pkg install jq
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo_info "Detected ${OSTYPE} ... Installing JQ for Mac OSX ..."
+    # Mac OSX
+    brew install jq
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+    echo_info "Detected ${OSTYPE} ... Installing JQ for Cygwin / Windows ..."
+    # POSIX compatibility layer and Linux environment emulation for Windows
+    chocolatey install jq
+  else
+    # Unknown.
+    echo_alert "ERROR: Unknown OS ${OSTYPE}"
+  fi
 else
-	# Unknown.
-	echo_alert "ERROR: Unknown OS ${OSTYPE}"
+  echo_info "jq Intalled already"
 fi
