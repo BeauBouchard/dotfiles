4af0b35a7af08ac65d1065ceec17c97329779867
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2021-10-28 13:25:51 -0400

    Update bash.sh

--- .shell/setup/install/bash.sh
+++ .shell/setup/install/bash.sh
@@ -2,7 +2,7 @@
 rm -Rf ~/.shell/  && \
 rm -Rf ~/.shell_helpers && \
 mkdir -p ~/.shell/setup/install && \
-mkdir -p ~/.shell/art && \
+mkdir -p ~/.shell/art/views && \
 cd ~/.shell && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/utils.sh ;} && \
 cd ~/.shell/art/views && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/art/views/home.sh ;} && \
 cd ~/.shell/art && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/art/bee.sh ;} && \
