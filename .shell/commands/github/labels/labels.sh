bd6a67fe02a3f7a8dcd908d97601a5e3ef942c02
Beau Bouchard <bnb2011@gmail.com>
2022-07-01 11:22:01 -0400

    fix: small changes to the script

--- .shell/commands/github/labels/labels.sh
+++ .shell/commands/github/labels/labels.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-## requires exturnal functions such as 
+## requires exturnal functions such as
 # echo_warn, echo_color, echo_color_n
-source ../../utils.sh
+source ~/.shell/utils.sh
 
 main() {
 	CAT=$(input_create_delete)
 	ORG=$(input_user "What org?")
 	REPO=$(input_user "What repo?")
 	echo_color $WHITE "org:${ORG} repo:${REPO} CAT:${CAT}";
-	# create_labels
+	if [ "$CAT" == "create" ]; then
+			echo_color $WHITE "Creating labels...";
+			create_labels $ORG $REPO;
+	elif [ "$CAT" == "delete" ]; then
+			echo_color $WHITE "Deleting labels...";
+			delete_labels $ORG $REPO;
+	else
+			echo_warn "Invalid input";
+			return 1;
+	fi
+
 }
 
 input_create_delete() {
@@ -17,10 +27,10 @@ input_create_delete() {
 		# get user's input and convert to lowercase with sed
 		local INPUT=$(input_user "Would you like to create labels or delete labels? [create/delete]" | sed -e "s/\(.*\)/\L\1/")
     case ${INPUT} in
-      create) s
+      create)
 				echo $INPUT
 				return 1 ;;
-      delete) 
+      delete)
 				echo $INPUT
 				return 2 ;;
       *) echo_warn "${INPUT} is invalid input, please type 'create' or 'delete'"
@@ -29,21 +39,21 @@ input_create_delete() {
 }
 
 create_labels() {
-	while IFS= read -r LABELNAME && IFS= read -r LABELCOLOR; do
+	while IFS= read -r LABELNAME && IFS= read -r LABELCOLOR && IFS= read -r LABELDESCRIPTION; do
 		echo_color_n $WHITE "["
 		echo_color_n $GREEN "CREATE"
-		echo_color_n $WHITE "][" 
+		echo_color_n $WHITE "]["
 		echo_color_n $GREEN "LABELS"
-		echo_color $WHITE "] Creating Labels for " 
-		echo_color_n $WHITE "Name: " 
+		echo_color $WHITE "] Creating Labels for "
+		echo_color_n $WHITE "Name: "
 		echo_color $CYAN "${LABELNAME}"
-		echo_color_n $WHITE "Description: " 
+		echo_color_n $WHITE "Description: "
 		echo_color $CYAN "${LABELDESCRIPTION}"
-		echo_color_n $WHITE "Color: " 
+		echo_color_n $WHITE "Color: "
 		echo_color $CYAN "${LABELCOLOR}"
 
-		create_label ${LABELNAME} ${LABELDESCRIPTION} ${LABELCOLOR}
-	done < <(jq -r "[] as $o | (.[$o].name, .[$o].description, .[$o].color)" $(path/abs)/labels.json)
+		create_label "${LABELNAME}" "${LABELDESCRIPTION}" "${LABELCOLOR}" "$1" "$2"
+	done < <(jq -r ".[] | .name, .color, .description" ./labels.json)
 }
 
 create_label() {
@@ -51,31 +61,31 @@ create_label() {
 		--header "Authorization: token ${GITHUBTOKEN}" \
 		--header "Content-Type: application/json" \
 		--data "{\"name\":\"$1\",\"description\":\"$2\", \"color\":\"$3\"}" \
-			"https://api.github.com/repos/${ORG}/${REPO}/labels"
+			"https://api.github.com/repos/${4}/${5}/labels"
 }
 
 get_labels() {
   LABELS=$(curl -X GET \
 		--header "Authorization: token ${GITHUBTOKEN}" \
 		--header "Content-Type: application/json" \
-		"https://api.github.com/repos/${ORG}/${REPO}/labels")
+		"https://api.github.com/repos/${1}/${2}/labels")
 
   LABELCOUNT=$(echo $LABELS | jq ". | length")
 }
 
 delete_labels() {
-	get_labels
+	get_labels $1 $2
   while IFS= read -r LABELNAME; do
 		echo_color_n $WHITE "["
 		echo_color_n $RED "DELETE"
-		echo_color_n $WHITE "][" 
+		echo_color_n $WHITE "]["
 		echo_color_n $RED "LABEL"
-		echo_color $WHITE "] Deleting Label " 
-		echo_color_n $WHITE "Name: " 
+		echo_color $WHITE "] Deleting Label "
+		echo_color_n $WHITE "Name: "
 		echo_color $CYAN "${LABELNAME}"
 
-    delete_label ${LABELNAME}
-  done < <(echo $LABELS | jq -r "[] as $o | (.[$o].name)")
+    delete_label "${LABELNAME}" $1 $2
+  done < <(echo $LABELS | jq -r ".[] | .name")
 }
 
 delete_label() {
@@ -84,7 +94,7 @@ delete_label() {
 	curl -X DELETE \
 		--header "Authorization: token ${GITHUBTOKEN}" \
 		--header "Content-Type: application/json" \
-		"https://api.github.com/repos/${ORG}/${REPO}/labels/$LABELNAME"
+		"https://api.github.com/repos/${2}/${3}/labels/$LABELNAME"
 }
 
 main
