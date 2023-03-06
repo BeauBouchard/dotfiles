f3ea7ee2172fe919961b88d5bd7b63bc6cb01e3c
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-03-06 12:49:28 -0500

    Update bash_home.sh

--- .shell/setup/install/bash_home.sh
+++ .shell/setup/install/bash_home.sh
@@ -7,10 +7,5 @@ echo "source ~/.shell/art/views/home.sh" >> ~/.bash_profile && \
 echo "source ~/.shell/art/views/home.sh" >> ~/.bashrc && \
 echo "display_home_motd" >> ~/.bash_profile && \
 echo "display_home_motd" >> ~/.bashrc && \ 
-## install ulauncher
-echo "installing ulauncher . . . " && \ 
-sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher  && \ 
-## install steam 
-echo "installing steam . . . " && \ 
-sudo add-apt-repository multiverse && sudo apt update  && sudo apt install steam  && \ 
+source ~/.shell/setup/install/steam.sh 
 source ~/.shell/setup/install/nvm.sh 
