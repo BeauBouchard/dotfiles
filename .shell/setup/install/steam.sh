2dabb0a320f14d73975140e5757ea9ac8d3aecff
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-03-06 15:18:57 -0500

    Update steam.sh

--- .shell/setup/install/steam.sh
+++ .shell/setup/install/steam.sh
@@ -3,26 +3,26 @@
 ## install steam 
 if ! command -v steam &> /dev/null; then
   if [[ "$OSTYPE" == "linux"* ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing Steam for Linux ..."
+    echo "Detected ${OSTYPE} ... Installing Steam for Linux ..."
     # linux and most distros
     sudo add-apt-repository multiverse && sudo apt update  && sudo apt install steam
   elif [[ "$OSTYPE" == "bsd"* ]]; then
   # elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing Steam for BSD ..."
+    echo "Detected ${OSTYPE} ... Installing Steam for BSD ..."
     # bsd
     pkg install steam
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing Steam for Mac OSX ..."
+    echo "Detected ${OSTYPE} ... Installing Steam for Mac OSX ..."
     # Mac OSX
     brew install steam
   elif [[ "$OSTYPE" == "cygwin" ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing Steam for Cygwin / Windows ..."
+    echo "Detected ${OSTYPE} ... Installing Steam for Cygwin / Windows ..."
     # POSIX compatibility layer and Linux environment emulation for Windows
     chocolatey install steam
   else
     # Unknown.
-    echo_alert "ERROR: Unknown OS ${OSTYPE}"
+    echo "ERROR: Unknown OS ${OSTYPE}"
   fi
 else
-  echo_info "Steam Intalled already"
+  echo "Steam Intalled already"
 fi
