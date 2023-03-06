44a44af03d70cee7ba6e03324a39486339d40f15
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-03-06 14:56:08 -0500

    Update bash_home.sh

--- .shell/setup/install/bash_home.sh
+++ .shell/setup/install/bash_home.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.shell/setup/install/ && \
 cd ~/.shell/setup/install && { curl -O -s https://raw.githubusercontent.com/BeauBouchard/dotfiles/main/.shell/setup/install/bash.sh ;} && \
 source ~/.shell/setup/install/bash.sh && \
-echo "source ~/.shell/art/views/home.sh" >> tee ~/.bash_profile ~/.bashrc && \
-echo "display_home_motd" >> tee ~/.bash_profile ~/.bashrc && \
+echo "source ~/.shell/art/views/home.sh" >> tee ~/.bash_profile | tee ~/.bashrc && \
+echo "display_home_motd" >> tee ~/.bash_profile | tee ~/.bashrc && \
 source ~/.shell/setup/install/steam.sh && \ 
 source ~/.shell/setup/install/ulauncher.sh && \ 
 source ~/.shell/setup/install/nvm.sh 
