63eb7a1117c3d9edb5c9a94f4a0c384a48242c52
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2023-03-06 15:15:12 -0500

    Update ulauncher.sh

--- .shell/setup/install/ulauncher.sh
+++ .shell/setup/install/ulauncher.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 
-source ~/.shell/utils.sh
-
 ## install ulauncher 
 if ! command -v ulauncher &> /dev/null; then
   if [[ "$OSTYPE" == "linux"* ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing ulauncher for Linux ..."
+    echo "Detected ${OSTYPE} ... Installing ulauncher for Linux ..."
     # linux and most distros
     sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
   elif [[ "$OSTYPE" == "bsd"* ]]; then
   # elif [[ "$OSTYPE" == "freebsd"* ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing ulauncher for BSD ..."
+    echo "Detected ${OSTYPE} ... Installing ulauncher for BSD ..."
     # bsd
     pkg install ulauncher
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing ulauncher for Mac OSX ..."
+    echo "Detected ${OSTYPE} ... Installing ulauncher for Mac OSX ..."
     # Mac OSX
     brew install ulauncher
   elif [[ "$OSTYPE" == "cygwin" ]]; then
-    echo_info "Detected ${OSTYPE} ... Installing ulauncher for Cygwin / Windows ..."
+    echo "Detected ${OSTYPE} ... Installing ulauncher for Cygwin / Windows ..."
     # POSIX compatibility layer and Linux environment emulation for Windows
     chocolatey install ulauncher
   else
     # Unknown.
-    echo_alert "ERROR: Unknown OS ${OSTYPE}"
+    echo "ERROR: Unknown OS ${OSTYPE}"
   fi
 else
-  echo_info "ulauncher Intalled already"
+  echo "ulauncher Intalled already"
 fi
